dee65e9e5bafa5fd29ccdc63084b91d827c2d37c
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-11-06 17:51:07 +0100

    Create create-template.tsx

---
+++ components/create-template.tsx
@@ -0,0 +1,65 @@
+import { Plus } from "lucide-react";
+import { Button } from "./ui/button";
+import dynamic from "next/dynamic";
+
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { useState } from "react";
+import { useTheme } from "next-themes";
+import { LanguageComboBox } from "./language-combobox";
+const CodeEditor = dynamic(
+  () => import("@uiw/react-textarea-code-editor").then((mod) => mod.default),
+  { ssr: false }
+);
+export default function CreateTemplate() {
+  const [template, setTemplate] = useState(``);
+  const { theme } = useTheme();
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <Button className="m-2 p-2 flex space-x-2 h-auto">
+          <Plus height={16} />
+          <p>Create</p>
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create a template</DialogTitle>
+          <DialogDescription>
+            Create a new template for generating documentations using the power
+            of generative artificial intelligence.
+          </DialogDescription>
+        </DialogHeader>
+        <h3 className="font-bold">Template</h3>
+        <CodeEditor
+          className="rounded-md border border-slate-200 dark:border-slate-600"
+          value={template}
+          language="markdown"
+          placeholder="Paste your markdown documentation template here."
+          onChange={(evn) => setTemplate(evn.target.value)}
+          padding={15}
+          data-color-mode={theme == "light" ? "light" : "dark"}
+          style={{
+            fontFamily:
+              "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+          }}
+        />
+        <h3 className="font-bold">Language</h3>
+        <LanguageComboBox />
+        <DialogFooter>
+          <DialogClose>
+            <Button>Create</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
