0521180a6858e02e361267d8989c648c2b078f07
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-11-18 19:04:09 +0100

    Improved landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,7 +8,17 @@ export default function Home() {
   return (
     <main>
       <section className="flex min-h-screen flex-col items-center justify-center">
-        <h1 className="text-5xl font-bold ">
+        <div className="rounded-2xl border my-2 dark:border-slate-800 bg-white/50 p-12 shadow-lg backdrop-blur-md transition hover:border-slate-400 hover:bg-white/70 hover:shadow-xl dark:bg-slate-800/20 dark:shadow-sky-500/20 dark:hover:border-slate-500 dark:hover:bg-slate-400/20">
+          <Image
+            height={96}
+            width={96}
+            alt="Synapsy Logo"
+            src={
+              useTheme().theme === "light" ? "logolight.svg" : "logodark.svg"
+            }
+          />
+        </div>
+        <h1 className="text-5xl text-center font-bold ">
           Create documentation easily with AI.
         </h1>
         <p className="text-slate-500">
