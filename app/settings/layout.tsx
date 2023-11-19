854ea118d8011dc96e8c449e8d4c1ec55043952f
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-11-19 11:54:32 +0100

    Improved layout on mobile devices

--- app/settings/layout.tsx
+++ app/settings/layout.tsx
@@ -25,9 +25,23 @@ export default function SettingsLayout({
 
   return (
     <main className="mt-16 mx-2">
-      <section className="mb-4">
-        <h3 className="font-bold text-2xl">Settings</h3>
-        <p>Customize Synapsy Genidoc.</p>
+      <section className="mb-4 grid grid-cols-[auto,1fr] items-center space-x-2">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="ghost" className="sm:hidden">
+              <Menu width={16} />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side={"left"}>
+            <SettingsNavBar
+              currentPage={pathname.split("/")[pathname.split("/").length - 1]}
+            />
+          </SheetContent>
+        </Sheet>
+        <div>
+          <h3 className="font-bold text-2xl">Settings</h3>
+          <p>Customize Synapsy Genidoc.</p>
+        </div>
       </section>
       <section className="grid grid-cols-[auto,1fr]">
         <div>
@@ -36,20 +50,6 @@ export default function SettingsLayout({
               currentPage={pathname.split("/")[pathname.split("/").length - 1]}
             />
           </span>
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button variant="outline" className="sm:hidden">
-                <Menu width={16} />
-              </Button>
-            </SheetTrigger>
-            <SheetContent side={"left"}>
-              <SettingsNavBar
-                currentPage={
-                  pathname.split("/")[pathname.split("/").length - 1]
-                }
-              />
-            </SheetContent>
-          </Sheet>
         </div>
         {children}
       </section>
