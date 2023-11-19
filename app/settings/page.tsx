831ceef779a7f9b326c66d0c575a3ce8befa3039
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-11-19 11:13:17 +0100

    Added settings page

---
+++ app/settings/page.tsx
@@ -0,0 +1,10 @@
+import { LucideMousePointer2 } from "lucide-react";
+
+export default function SettingsPage() {
+  return (
+    <div className="min-h-[50vh] flex flex-col items-center justify-center">
+      <LucideMousePointer2 />
+      <p>Select an item on the left to get started.</p>
+    </div>
+  );
+}
