4c62625ae4cc063174e7edba3ff118785832d2ba
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-05-04 11:54:07 +0200

    Added desktop navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,51 +2,40 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import Link from "next/link";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "./ui/dropdown-menu";
-import { Sun, Moon } from "lucide-react";
-import { useTheme } from "next-themes";
+import { Rocket, Settings } from "lucide-react";
 
 export default function NavBar() {
-  const { setTheme } = useTheme();
   return (
-    <div className="fixed top-0 z-50 flex w-full items-center justify-center print:hidden">
-      <nav className="m-2 flex items-center rounded-full bg-white/50 px-4 py-2 shadow-md backdrop-blur-md dark:bg-slate-900/50">
+    <nav className="fixed top-0 z-50 hidden w-full grid-cols-[auto,1fr,auto] bg-white/50 px-4 py-1 shadow-sm saturate-[1.1] backdrop-blur-md dark:bg-slate-900/30 sm:grid print:hidden">
+      <div className="m-2 flex items-center">
         <Link className="mr-2 flex space-x-2" href={"/"}>
           <Image alt="SynapsyLogo" height={25} width={25} src={"/logo.svg"} />
-          <h2 className="font-bold">GeniDoc</h2>
+          <h2 className="text-lg font-normal">Genidoc</h2>
         </Link>
-        <Link href={"/create"}>
-          <Button variant="ghost">Create</Button>
-        </Link>
+
         <Link href={"/generations"}>
           <Button variant="ghost">My Generations</Button>
         </Link>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" size="icon">
-              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-              <span className="sr-only">Toggle theme</span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem onClick={() => setTheme("light")}>
-              Light
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme("dark")}>
-              Dark
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme("system")}>
-              System
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </nav>
-    </div>
+      </div>
+      <span></span>
+      <div className="m-2 flex items-center">
+        <Link href={"/create"}>
+          <Button className="group mx-2 h-auto space-x-2 overflow-hidden px-2 py-1">
+            <Rocket
+              className="group-hover:animate-rocket"
+              height={14}
+              width={14}
+            />
+            <p>Create</p>
+          </Button>
+        </Link>
+
+        <Link href={"/settings"} className="group">
+          <Button variant="ghost" size="icon">
+            <Settings className="h-[1.2rem] w-[1.2rem] transition-all group-hover:rotate-90" />
+          </Button>
+        </Link>
+      </div>
+    </nav>
   );
 }
