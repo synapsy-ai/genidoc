f595322d31df0a4f54ff093a4a96958a9acf9dff
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-07-27 14:15:22 +0200

    Added "Learn more" link

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
                     <Button>Get started</Button>
                   </Link>
                   <Link
-                    href="#"
+                    href="#features"
                     className="inline-flex h-10 items-center justify-center rounded-md border border-input bg-background px-8 text-sm font-medium shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                     prefetch={false}
                   >
